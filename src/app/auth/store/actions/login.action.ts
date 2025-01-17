65f762878159bf98c46540694b51ea0e244fb199
Maksym Pastukh <saponexamq@gmail.com>
2025-01-17 09:31:33 +0100

    Добавил Effect для логина

--- src/app/auth/store/actions/login.action.ts
+++ src/app/auth/store/actions/login.action.ts
@@ -4,17 +4,17 @@ import {LoginRequestInterface} from '../../types/loginRequest.interface'
 import {CurrentUserInterface} from '../../../shared/types/currentUser.interface'
 import {BackendErrorsInterface} from '../../../shared/types/backendErrors.interface'
 
-export const LoginAction = createAction(
+export const loginAction = createAction(
   ActionTypes.LOGIN,
   props<{request: LoginRequestInterface}>()
 )
 
-export const LoginSuccessAction = createAction(
+export const loginSuccessAction = createAction(
   ActionTypes.LOGIN_SUCCESS,
   props<{currentUser: CurrentUserInterface}>()
 )
 
-export const LoginFailureActions = createAction(
+export const loginFailureActions = createAction(
   ActionTypes.LOGIN_FAILURE,
-  props<{errors: BackendErrorsInterface}>
+  props<{errors: BackendErrorsInterface}>()
 )
---
+++ src/app/auth/store/effects/login.effect.ts
@@ -0,0 +1,43 @@
+import {Actions, createEffect, ofType} from '@ngrx/effects'
+import {inject} from '@angular/core'
+import {AuthService} from '../../services/auth.service'
+import {PersistanceService} from '../../../shared/services/persistance.service'
+import {loginAction, loginFailureActions, loginSuccessAction} from '../actions/login.action'
+import {catchError, map, of, switchMap, tap} from 'rxjs'
+import {CurrentUserInterface} from '../../../shared/types/currentUser.interface'
+import {HttpErrorResponse} from '@angular/common/http'
+import {Router} from '@angular/router'
+
+export const loginEffect = createEffect(
+  (actions$ = inject(Actions),
+  authService = inject(AuthService),
+  persistenceService = inject(PersistanceService)) => {
+    return actions$.pipe(
+      ofType(loginAction),
+      switchMap(({request}) => {
+        return authService.login(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            persistenceService.setItem('accessToken', currentUser.token)
+            return loginSuccessAction({currentUser})
+          }),
+          catchError((errorResponse: HttpErrorResponse)=> {
+            return of(loginFailureActions({errors: errorResponse.error.errors}))
+          })
+        )
+      })
+    )
+  },
+  {functional: true}
+)
+
+export const redirectAfterSubmit = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => actions$.pipe(
+    ofType(loginSuccessAction),
+    tap(() => {
+      router.navigate(['/'])
+    })
+  ), {
+    dispatch: false,
+    functional: true
+  }
+)
