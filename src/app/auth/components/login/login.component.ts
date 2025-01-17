b8fa2ec9a0fab1099d713b460cc5d5ffc74db0e5
Maksym Pastukh <saponexamq@gmail.com>
2025-01-17 08:58:59 +0100

    Реалезована форма и валидация для логина

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
 import {RouterLink} from '@angular/router'
 import {CommonModule} from '@angular/common'
 import {Button} from 'primeng/button'
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
 
 @Component({
   selector: 'c-login',
   imports: [
     CommonModule,
     RouterLink,
-    Button
+    Button,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  public formSingIn: FormGroup
 
+  constructor(private fb: FormBuilder) {
+  }
+
+
+  get email(): any {
+    return this.formSingIn.get('email')
+}
+
+
+  ngOnInit() {
+    this.initializeForm()
+  }
+
+
+  initializeForm() : void {
+    this.formSingIn = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['']
+    })
+  }
+
+  onSingIn() : void {
+
+  }
 }
