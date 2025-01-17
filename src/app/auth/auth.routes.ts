f2c76613000f6b5c6bf31fd19a9590555a123f54
Maksym Pastukh <saponexamq@gmail.com>
2025-01-17 08:27:57 +0100

    Добавлен новый компонент для логина и верстка к нему

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -5,6 +5,10 @@ export default [
     path: 'register',
     loadComponent: () =>
       import('./components/register/register.component').then(c => c.RegisterComponent),
+  }, {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(c => c.LoginComponent),
   },
 ] as Routes;
 
